70dc08f377c161c5f734160d7f1708d40814e1a2
Trufflz <spotandroxy3@gmail.com>
2023-10-30 17:08:45 -0700

    fixing registration

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,61 +1,84 @@
-import { useState } from 'react'
-import './Login.css'
-import banner from '../images/ucr-banner.png'
-
-/* Extra Features
-    ** Incorrect Email or Password Check?
-    1) Email already exists in db alert when trying to register
-    2) Forgot Password?
-    3) CSS Prettying [see Login.css for more info]
-*/
+import { useState } from "react";
+import "./Login.css";
+import banner from "../images/ucr-banner.png";
 
 function Login() {
-    //State Hooks
-    const [title, setTitle] = useState('Login');
-    const [buttonName, setButtonName] = useState('Sign In');
+  //State Hooks
+  const [title, setTitle] = useState("Login");
+  const [buttonName, setButtonName] = useState("Sign In");
 
-    //Event Handlers
-    function handleFormSubmit() {
-        if (title != 'Login') { handleReset(); }
+  //Event Handlers
+  function handleFormSubmit() {
+    if (title != "Login") {
+      handleReset();
     }
-    function handleRegister() {
-        setTitle('Register');
-        setButtonName('Sign Up');
-    }
-    function handleReset() {
-        setTitle('Login');
-        setButtonName('Sign In');
-    }
+  }
+  function handleRegister() {
+    setTitle("Register");
+    setButtonName("Sign Up");
+  }
+  function handleReset() {
+    setTitle("Login");
+    setButtonName("Sign In");
+  }
 
+  return (
+    <>
+      <div className="image">
+        <a href="#" onClick={() => handleReset()}>
+          <img src={banner}></img>
+        </a>
+      </div>
+      <section>
+        <div className="login-container">
+          <div className="login-title">
+            <h1>{title}</h1>
+          </div>
 
-    return (
-        <>
-            <div className='image'>
-                <a href='#' onClick={() => handleReset()}><img src={banner}></img></a>
-            </div>
-            <section>
-                <div className='login-container'>
-                    <div className='login-title'>
-                        <h1>{title}</h1>
-                    </div>
-
-                    <div className='login-contents'>
-                        <form>
-                            <div className='input-boxes'>
-                                <input name='email' type='text' placeholder='Email' required></input>   
-                            </div>
-                            <div className='input-boxes'>
-                                <input name='pwd' type='password' placeholder='Password' required></input>
-                            </div>
-                            <button className='login-submit' onSubmit={() => handleFormSubmit()}>{buttonName}</button>
-                            {title === 'Login'? <p className='register'>Don&apos;t have an account?&nbsp;<a href='#' onClick={()=>handleRegister()}>Register</a></p> : <p></p>}
-                        </form>
-                    </div>
-
+          <div className="login-contents">
+            <form>
+              <div className="input-boxes">
+                <input
+                  name="email"
+                  type="text"
+                  placeholder="Email"
+                  required
+                ></input>
+              </div>
+              <div className="input-boxes">
+                <input
+                  name="pwd"
+                  type="password"
+                  placeholder="Password"
+                  required
+                ></input>
+              </div>
+              <button
+                className="login-submit"
+                onSubmit={() => handleFormSubmit()}
+              >
+                {buttonName}
+              </button>
+              {title === "Login" ? (
+                <p className="register">
+                  Don&apos;t have an account?&nbsp;
+                  <a href="#" onClick={() => handleRegister()}>
+                    Register
+                  </a>
+                </p>
+              ) : (
+                <div>
+                  <input type='radio' id='student'></input>
+                  <input type='radio' id='professor'></input>
                 </div>
-            </section>
-        </>
-    )
+
+              )}
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Login
+export default Login;
