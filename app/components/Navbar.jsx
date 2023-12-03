03cb74e6be294206fe2de60051f085bfb8fa1889
Adlai M.B <adlaimb@gmail.com>
2023-12-02 20:43:11 -0800

    fix up navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,33 +1,33 @@
 "use client"; //makes page interactable with user instead of just rendering on server
 
-import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Navbar.module.css'
 //import {redirect} from 'next/navigation'
 
+function returnToLogin() {
+    //sign out user
+    //reroute to login page
+    location.href = '/';
+}
+
 export default function Navbar() {
-
-    function handleClick() {
-        //sign out user
-        //reroute to login page
-        location.href = '/';
-        
-    }
-
-    return(
-        <div className={styles.navbar}>
-            <Image className={styles.image} src="/ucr_logo.png" width="210" height="64"></Image>
-            <nav>
-                <Link className={styles.links} href='/'>Login</Link>
-                <Link className={styles.links} href='/student/job-listings'>Job Listings</Link>
-                <Link className={styles.links} href='/student/rec-jobs'>Recommended Jobs</Link>
-                <Link className={styles.links} href='/student/applied-jobs'>Applied Jobs</Link>
-                <Link className={styles.links} href='/student/profile'>Profile</Link>
+    return (
+        <header className={styles.navbar_container}>
+            <a href="/">
+                <Image className={styles.img} src="/ucr_logo.png" width="210" height="64"></Image>
+            </a>
+            <nav className={styles.navbar}>
+                <ul className={`${styles.navbar_list} ${styles.navbar_list_primary}`}>
+                    <li className={styles.navbar_item}><a className={styles.navbar_link} href="/student/job-listings">Job Listings</a></li>
+                    <li className={styles.navbar_item}><a className={styles.navbar_link} href="/student/profile">Profile</a></li>
+                    <li className={styles.navbar_item}><a className={styles.navbar_link} href="/student/rec-jobs">Recommended  Jobs</a></li>
+                    <li className={styles.navbar_item}><a className={styles.navbar_link} href="/student/applied-jobs">Applied Jobs</a></li>
+                </ul>
+                <ul className={styles.navbar_list}>
+                    <li className={styles.navbar_item}><a className={styles.navbar_link} href="/student/profile">Current User</a></li>
+                    <button onClick={returnToLogin} className={`${styles.button} ${styles.bg_blue}`}>Log out</button>
+                </ul>
             </nav>
-                <div className={styles.signout}>
-                    <p>Hello [Current User]</p>
-                    <button className={styles.button} onClick={handleClick}>Sign Out</button>
-                </div>
-        </div>
+        </header>
     )
 }
