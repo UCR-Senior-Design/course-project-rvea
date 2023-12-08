ea289c82058c007bb3ee2858f6736cdd98ec1dfa
Vincent <spotandroxy3@gmail.com>
2023-12-07 23:49:59 -0800

    reorganized navbar

--- app/professor/layout.jsx
+++ app/professor/layout.jsx
@@ -11,4 +11,8 @@ export default function ProfessorLayout({ children }) {
     )
 }
 
-//Fix Navbar to pass in prop routes?
+//Fix Navbar to pass in prop routes in layout page for student and professor?
+/*
+1) pass in a props list (in that list is the route_name(1) and the route_link(2))
+2) create list items individually in a loop
+*/
