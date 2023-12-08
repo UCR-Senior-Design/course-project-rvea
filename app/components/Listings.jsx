434666e8cd826bcd570d04ba8f653be0f6c5087b
Vincent <spotandroxy3@gmail.com>
2023-12-07 19:39:32 -0800

    Added Job Listings Details w/ Clickability

--- app/components/Listings.jsx
+++ app/components/Listings.jsx
@@ -1,93 +1,27 @@
+'use client'
 import styles from '../styles/Listings.module.css'
+import {useState} from 'react'
 import Image from 'next/image'
 
-const dummyData = [{
-    title: 'Job Title 1',
-    name: 'Professor _____',
-    quarter: 'Winter and Spring Quarter',
-    wage: '$120/hour',
-    seats: '5',
-},
-{
-    title: 'Job Title 2',
-    name: 'Professor _____',
-    quarter: 'Winter and Spring Quarter',
-    wage: '$120/hour',
-    seats: '5',
-},
-{
-    title: 'Job Title 3',
-    name: 'Professor _____',
-    quarter: 'Winter and Spring Quarter',
-    wage: '$120/hour',
-    seats: '5',
-},
-{
-    title: 'Job Title 4',
-    name: 'Professor _____',
-    quarter: 'Winter and Spring Quarter',
-    wage: '$120/hour',
-    seats: '5',
-},
-{
-    title: 'Job Title 5',
-    name: 'Professor _____',
-    quarter: 'Winter and Spring Quarter',
-    wage: '$120/hour',
-    seats: '5',
-},
-{
-    title: 'Job Title 6',
-    name: 'Professor _____',
-    quarter: 'Winter and Spring Quarter',
-    wage: '$120/hour',
-    seats: '5',
-},
-{
-    title: 'Job Title 7',
-    name: 'Professor _____',
-    quarter: 'Winter and Spring Quarter',
-    wage: '$120/hour',
-    seats: '5',
-},
-{
-    title: 'Job Title 8',
-    name: 'Professor _____',
-    quarter: 'Winter and Spring Quarter',
-    wage: '$120/hour',
-    seats: '5',
-},
-{
-    title: 'Job Title 9',
-    name: 'Professor _____',
-    quarter: 'Winter and Spring Quarter',
-    wage: '$120/hour',
-    seats: '5',
-},
-{
-    title: 'Job Title 10',
-    name: 'Professor _____',
-    quarter: 'Winter and Spring Quarter',
-    wage: '$120/hour',
-    seats: '5',
-}
-];
 
-export default function Listings() {
+export default function Listings(props) {
+    const listings = props.jobListings;
+    const [jobIndex, setJobIndex] = useState(0);
+
     return (
         <div className={styles.container}>
 
             <div className={styles['listings-container']}>
-                    {dummyData.map((data, index) => {
+                    {listings.map((data, index) => {
                         return (
-                            <div className={styles.cards} key={index}>
+                            <div className={styles.cards} key={index} onClick={() => setJobIndex(index)}>
                                 <Image className={styles['card-logo']} src='/pc.png' width='60' height='60'></Image>
                                 <div>
-                                    <h3 className={`${styles.text} ${styles.title}`}>{data.title}</h3>
-                                    <p className={`${styles.text} ${styles.name}`}>{data.name}</p><br></br>
-                                    <p className={`${styles.text} ${styles.quarter}`}>{data.quarter}</p>
-                                    <p className={styles.text}>Wage: <span className={styles.wage}>{data.wage}</span></p>
-                                    <p className={styles.text}>Remaining Seats: <span className={styles.seats}>{data.seats}</span></p>
+                                    <h3 className={`${styles.text} ${styles.title}`}>{data.jobTitle}</h3>
+                                    <p className={`${styles.text} ${styles.name}`}>{data.professor}</p><br></br>
+                                    <p className={`${styles.text} ${styles.quarter}`}>Term: {data.jobTerm}</p>
+                                    <p className={styles.text}>Wage: <span className={styles.wage}>{data.hourlyWage}</span></p>
+                                    <p className={styles.text}>Remaining Seats: <span className={styles.seats}>{data.totalSeats}</span></p>
                                 </div>
                             </div>
                         )
@@ -95,41 +29,37 @@ export default function Listings() {
             </div>
 
 
-
             <div className={styles['details-container']}>
                 <div className={styles.details}>
-                    <h1>CS111-Discrete Structures Teaching Assistant</h1>
-                    
+
+                    <h1>{listings[jobIndex].jobTitle}</h1>
                     <div className={styles.subtitle}>
                         <Image className={styles.no_margin} src='/pc.png' width='60' height='60'></Image>
                         <div>
-                            <p className={`${styles.subtitle_text} ${styles.color_bold}`}>Elena Strzheletska</p>
-                            <p className={`${styles.subtitle_text} ${styles.color_gray} ${styles.no_margin}`}>Term: Spring 2024</p>
+                            <p className={`${styles.subtitle_text} ${styles.color_bold}`}>{listings[jobIndex].professor}</p>
+                            <p className={`${styles.subtitle_text} ${styles.color_gray} ${styles.no_margin}`}>Term: {listings[jobIndex].jobTerm}</p>
                         </div>
-
                     </div>
 
-                    
-                    
                     <h2 className={`${styles.underline} ${styles.no_margin}`}>About the Role</h2>
                     <div className={styles.about}>
                         <div className={styles.about_card}>
                             <p className={`${styles.about_heading} ${styles.color_bold}`}>Deadline to Apply</p>
-                            <p className={`${styles.no_margin} ${styles.color_gray}`}>one two three four five six seven eight nine ten</p>
+                            <p className={`${styles.no_margin} ${styles.color_gray}`}>{listings[jobIndex].deadline}</p>
                         </div>
                         <div className={styles.about_card}>
                             <p className={`${styles.about_heading} ${styles.color_bold}`}>Wage</p>
-                            <p className={`${styles.no_margin} ${styles.color_gray}`}>one two three four five six seven eight nine ten</p>
+                            <p className={`${styles.no_margin} ${styles.color_gray}`}>{listings[jobIndex].hourlyWage}/hour</p>
                         </div>
                         <div className={styles.about_card}>
                             <p className={`${styles.about_heading} ${styles.color_bold}`}>Minimum Hours/Week</p>
-                            <p className={`${styles.no_margin} ${styles.color_gray}`}>one two three four five six seven eight nine ten</p>
+                            <p className={`${styles.no_margin} ${styles.color_gray}`}>{listings[jobIndex].minHrs} hours/week</p>
                         </div>        
                     </div>
-                    <p className={styles.prereq_text}><span className={styles.color_bold}>Prerequisite(s): </span><span className={styles.color_gray}>asdfasdf</span></p>
+                    <p className={styles.prereq_text}><span className={styles.color_bold}>Prerequisite(s): </span><span className={styles.color_gray}>{listings[jobIndex].prerequisites}</span></p>
 
                     <h2 className={`${styles.underline} ${styles.no_margin}`}>Role Description</h2>
-                    <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pharetra maximus rutrum. Nunc ac congue turpis. Aenean gravida eget massa quis luctus. Integer neque nibh, efficitur ut sollicitudin eu, finibus at felis. Sed rutrum velit eu velit venenatis, et venenatis metus volutpat. Morbi vestibulum, quam eget elementum accumsan, velit dui elementum sem, sed varius sem metus eget diam. Maecenas dolor odio, porttitor sit amet purus facilisis, tristique vulputate lorem. Integer nec enim nisl. Praesent sed eleifend nisi. Donec id efficitur lectus.</p>
+                    <p className={styles.description}>{listings[jobIndex].jobDescription}</p>
                     <button className={`${styles.button} ${styles.color_bold}`}>Apply</button>
                 </div>
             </div>
@@ -137,6 +67,7 @@ export default function Listings() {
     )
 }
 
-//useState on click?
-//FIXME: overflow scroll not working for details container
+
+//*display green container upon submission?
 //Fixme: scrollbar on outside of details container? (nit picky)
+//pfp json data is defaultly included in span?
--- app/student/job-listings/page.jsx
+++ app/student/job-listings/page.jsx
@@ -1,14 +1,20 @@
 import Searchbar from '../../components/Searchbar.jsx'
 import Listings from '../../components/Listings.jsx'
+import { promises as fs } from "fs";
 
-export default function JobListings() {
+async function getJobListingsMockData() {
+    const file = await fs.readFile(process.cwd() + "/json/jobListingsMockData.json", "utf-8");
+    const data = JSON.parse(file);
+    return data.jobListings;
+}
+
+
+export default async function JobListings() {
+    const jobListings = await getJobListingsMockData(); 
     return(
         <>
             <Searchbar />
-            <Listings />
+            <Listings jobListings={jobListings}/>
         </>
-
     )
 }
-
-/*useState for displaying details for each class*/
