644d9c7fc29f225389a773467152dcdfcd3a8ecf
Adlai M.B <adlaimb@gmail.com>
2023-12-10 22:51:54 -0800

    add hover actions on loginForm

--- app/components/LoginForm.jsx
+++ app/components/LoginForm.jsx
@@ -16,9 +16,9 @@ export default function LoginForm() {
                     <h2 className={styles.bold}>Sign in</h2>
                     <input id="email" name="email" className={`${styles.border_rounded_8} ${styles.border_grey}`} placeholder="Email" type="text"></input>
                     <input id="password"  name="password" className={`${styles.border_rounded_8} ${styles.border_grey}`} placeholder="Password" type="password"></input>
-                    <button className={`${styles.button} ${styles.bg_blue} ${styles.margin_top_24}`}> Sign in </button>
+                    <button className={`${styles.button_blue} ${styles.margin_top_24}`}> Sign in </button>
                     <h5 className={styles.line_through}><span>or</span></h5>
-                    <button className={`${styles.button} ${styles.bg_yellow} ${styles.margin_btm_24}`}>Register Now</button>
+                    <button className={`${styles.button_yellow} ${styles.margin_btm_24}`}>Register Now</button>
                 </div>
             </form>
         </>
