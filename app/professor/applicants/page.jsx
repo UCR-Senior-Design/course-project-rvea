b00d4723eece508a9ff039d1fc266016301f1d31
Vincent <spotandroxy3@gmail.com>
2024-02-01 01:23:56 -0800

    added expanding

--- app/professor/applicants/page.jsx
+++ app/professor/applicants/page.jsx
@@ -1,5 +1,97 @@
+import styles from '../../styles/applicants.module.css'
+
 export default function Applicants() {
     return (
-        <h1> Applicants</h1>
+        <div className={styles.container}>
+
+            <div className={styles.course}>
+                <h1 className={styles.underline}>CS170 - Intro to AI</h1>
+
+                <div className={styles.labels}>
+                    <h4>Name</h4>
+                    <h4>Degree</h4>
+                    <h4>GPA</h4>
+                    <h4>Application Date</h4>
+                    <h4>Transcript</h4>
+                    <h4>Status</h4>
+                </div>
+
+                <ul className={styles.accordion}>
+                    <li>
+                        <input type='checkbox' name='accordion' id='first' />
+                        <label for='first'>Ethan Fox</label>
+
+                        <div className={styles.content}>
+                            <p className={styles.bold}>Pronouns:</p>
+                            <p className={styles.bold}>Courses Taken: </p>
+                            <p className={styles.bold}>Skills:</p>
+                            <p className={styles.bold}>Resume:</p>
+                        </div>
+                    </li>
+                </ul>
+
+            </div>
+
+
+
+            <div className={styles.course}>
+                <h1 className={styles.underline}>CS170 - Intro to AI</h1>
+
+                <div className={styles.labels}>
+                    <h4>Name</h4>
+                    <h4>Degree</h4>
+                    <h4>GPA</h4>
+                    <h4>Application Date</h4>
+                    <h4>Transcript</h4>
+                    <h4>Status</h4>
+                </div>
+
+                <ul className={styles.accordion}>
+                    <li>
+                        <input type='checkbox' name='accordion' id='second' />
+                        <label for='second'>Ethan Fox</label>
+
+                        <div className={styles.content}>
+                            <p className={styles.bold}>Pronouns:</p>
+                            <p className={styles.bold}>Courses Taken: </p>
+                            <p className={styles.bold}>Skills:</p>
+                            <p className={styles.bold}>Resume:</p>
+                        </div>
+                    </li>
+                </ul>
+
+
+                <ul className={styles.accordion}>
+                    <li>
+                        <input type='checkbox' name='accordion' id='third' />
+                        <label for='third'>Ethan Fox</label>
+
+                        <div className={styles.content}>
+                            <p className={styles.bold}>Pronouns:</p>
+                            <p className={styles.bold}>Courses Taken: </p>
+                            <p className={styles.bold}>Skills:</p>
+                            <p className={styles.bold}>Resume:</p>
+                        </div>
+                    </li>
+                </ul>
+
+                <ul className={styles.accordion}>
+                    <li>
+                        <input type='checkbox' name='accordion' id='fourth' />
+                        <label for='fourth'>Ethan Fox</label>
+
+                        <div className={styles.content}>
+                            <p className={styles.bold}>Pronouns:</p>
+                            <p className={styles.bold}>Courses Taken: </p>
+                            <p className={styles.bold}>Skills:</p>
+                            <p className={styles.bold}>Resume:</p>
+                        </div>
+                    </li>
+                </ul>
+
+            </div>
+
+
+        </div>
     )
 }
