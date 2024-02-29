7cb29adf56ce4b468c6809353bff2b000da0846b
Adlai M.B <adlaimb@gmail.com>
2024-02-28 19:57:19 -0800

    add user interactivity

--- app/professor/profile/page.jsx
+++ app/professor/profile/page.jsx
@@ -1,6 +1,13 @@
+'use client'
+
+import { useState } from 'react';
 import styles from '../../styles/ProfessorProfile.module.css'
 
 export default function Profile() {
+
+    const [mode, setMode] = useState(1);
+    const [mode2, setMode2] = useState(1);
+
     return (
         <div className={styles.profileContainer}>
             <div>
@@ -8,22 +15,27 @@ export default function Profile() {
                     <div className={`${styles.col_entry} ${styles.card}`}>
                         <div className={styles.card_top}>
                             <h3>General & Contact Info</h3>
-                            <button className={styles.btn}>+</button>
+                            <button className={styles.btn} onClick={() => setMode(mode == 0 ? 1 : 0)}>+</button>
                         </div>
-                        <h5>Email: </h5>
-                        <h5>Phone Number: </h5>
-                        <h5>Pronouns: </h5>
+                        <h5 className={mode ? styles.nothidden : styles.hidden}>Email: </h5>
+                        <h5 className={mode ? styles.nothidden : styles.hidden}>Phone Number: </h5>
+                        <h5 className={mode ? styles.nothidden : styles.hidden}>Pronouns: </h5>
+                        <input className={`${styles.block} ${!mode && styles.nothidden} ${mode && styles.hidden}`} placeholder='Email'></input>
+                        <input className={`${styles.block} ${!mode && styles.nothidden} ${mode && styles.hidden}`} placeholder='Phone Number'></input>
+                        <input className={`${styles.block} ${!mode && styles.nothidden} ${mode && styles.hidden}`} placeholder='Pronouns'></input>
                     </div>
                 </div>
                 <div className={styles.col}>
                     <div className={`${styles.col_entry} ${styles.card}`}>
                         <div className={styles.card_top}>
                             <h3>Description</h3>
-                            <button className={styles.btn}>+</button>
+                            <button className={styles.btn} onClick={() => setMode2(mode2 == 0 ? 1 : 0)}>+</button>
                         </div>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda obcaecati eius
+                        <p className={mode2 ? styles.nothidden : styles.hidden}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda obcaecati eius
                             ducimus ad sequi a aliquam incidunt quo impedit, adipisci minima tempora nostrum
-                            fugit accusamus? Aspernatur quibusdam quae quam pariatur.</p>
+                            fugit accusamus? Aspernatur quibusdam quae quam pariatur.
+                        </p>
+                        <textarea className={`${!mode2 && styles.nothidden} ${mode2 && styles.hidden}`} name="description" id="description" cols="88" rows="10" placeholder='Enter your Description...'></textarea>
                     </div>
                 </div>
             </div>
