3a2fd2aca0ca28b1adf40301febf02df8c4535c5
lay6351 <rlayc001@ucr.edu>
2024-03-08 21:02:47 -0800

    updated profile page
    can upload pdfs from computer, each section is expandable, push and pop function for adding skills/classes/etc

--- app/components/ProfilePage.jsx
+++ app/components/ProfilePage.jsx
@@ -1,12 +1,31 @@
 'use client';
-import {useState} from "react";
-import {addNewSkill, removeSkill} from "./ProfilePageFunctions";
+import {useState, useRef} from "react";
+import {addNewSkill, removeSkill, addNewCourse, removeCourse, addYear, removeYear, addExperience, removeExperience, addNewPersonalInfo, removePersonalInfo, handleFileChange} from "./ProfilePageFunctions";
 
 import styles from "../styles/ProfilePage.module.css";
 
 
 export default function ProfilePage() {
     const [skills, setSkills] = useState(['C++', 'Python', 'Github/Git']);
+    const [courses, setCourses] = useState(['A', 'B', 'C']);
+    const [years, setYears] = useState([]);
+    const [experiences, setExperiences] = useState(['Engineered a full-stack web application for a university project using React for the frontend and Node.js for the backend, implementing RESTful APIs and real-time data with WebSocket.', 'Contributed to an open-source machine learning project on GitHub, enhancing algorithms and optimizing data processing pipelines in Python.', 'Led a team in a national coding hackathon to develop a mobile app prototype that won second place for its innovative use of AI in educational technology.']);
+    const [personal_infos, setPersonal_Infos] = useState(['Name: First Last', 'GPA: 5.0', 'Occupation: Student']);
+    const [pdfs, setPdfs] = useState([]);
+
+    const fileInputRef = useRef(null);
+
+    const triggerFileInputClick = () => fileInputRef.current.click();
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file && file.type === "application/pdf") {
+            const newPdfUrl = URL.createObjectURL(file);
+            setPdfs(prevPdfs => [...prevPdfs, newPdfUrl]);
+        }
+      };
+      
+
 
     return(
         <div className={styles.container}>
@@ -29,23 +48,34 @@ export default function ProfilePage() {
                 </div>
                 <div className={styles['courses_container']}>
                     <div className={styles['courses_upper_container']}>
-                        <h1>Courses</h1>
-                        <button className={`${styles.button} ${styles.bg_yellow}`}>+</button>
+                        <div className={styles['courses_upper_left_container']}>
+                            <h1>Courses</h1>
+                        </div>
+                        <div className={styles['courses_upper_right_container']}>
+                            <button onClick={() => removeCourse(courses, setCourses)} className={`${styles.button} ${styles.bg_yellow}`}>-</button>
+                            <button onClick={() => addNewCourse(courses, setCourses)} className={`${styles.button} ${styles.bg_yellow}`}>+</button> 
+                        </div>
                     </div>
                     <div className={styles['courses_lower_container']}>
-                    <button className={`${styles.pill}`}>C++</button>
-                            <button className={`${styles.pill}`}>CS 100</button>
-                            <button className={`${styles.pill}`}>STAT 156A</button>
-                            <button className={`${styles.pill}`}>CS 101</button>
+                        {courses.map((course, index) => (
+                            <button key={index} className={`${styles.pill}`}>{course}</button>
+                        ))}
                     </div>
                 </div>
                 <div className={styles['year_container']}>
-                    <div className={styles['courses_upper_container']}>
-                        <h1>Year</h1>
-                        <button className={`${styles.button} ${styles.bg_yellow}`}>+</button>
+                    <div className={styles['year_upper_container']}>
+                        <div className={styles['year_upper_left_container']}>
+                            <h1>Graduation Year</h1>
+                        </div>
+                        <div className={styles['courses_upper_right_container']}>
+                            <button onClick={() => removeYear(years, setYears)} className={`${styles.button} ${styles.bg_yellow}`}>-</button>
+                            <button onClick={() => addYear(years, setYears)} className={`${styles.button} ${styles.bg_yellow}`}>+</button>
+                        </div>
                     </div>
-                    <div className={styles['courses_lower_container']}>
-                            <button className={`${styles.pill}`}>2024</button>
+                    <div className={styles['year_lower_container']}>
+                        {years.map((year, index) => (
+                            <button key={index} className={`${styles.pill}`}>{year}</button>
+                        ))}
                     </div>                
                 </div>
             </div>
@@ -53,34 +83,60 @@ export default function ProfilePage() {
                 <div className={styles['resume_container']}>
                     <div className={styles['resume_upper_container']}>
                         <h1>Resume</h1>
-                        <button className={`${styles.button} ${styles.bg_yellow}`}>+</button>
+                        <button onClick={() => triggerFileInputClick()} className={`${styles.button} ${styles.bg_yellow}`}>+</button>
+                        <input
+                            type="file"
+                            ref={fileInputRef}
+                            style={{ display: 'none' }}
+                            accept="application/pdf"
+                            onChange={handleFileChange} // Trigger this function when a file is selected
+                        />
                     </div>
                     <div className={styles['resume_lower_container']}>
-                        <iframe src="/public/College Student Part-Time Job Resume Template.pdf" frameborder="0"></iframe>
+                        <div className={styles['resume_lower_lower_container']}>
+                            {pdfs.map((pdfPath, index) => (
+                                <iframe 
+                                key={index}
+                                src={pdfPath}
+                                style={{ width: '100%', height: '750px', border: 'none' }}
+                                frameBorder="0"
+                                ></iframe>
+                            ))}
+                        </div>
                     </div>
                 </div>               
                 <div className={styles['relevant_experience_container']}>
                     <div className={styles['relevant_experience_upper_container']}>
-                        <h1>Relevant Experience</h1>
-                        <button className={`${styles.button} ${styles.bg_yellow}`}>+</button>
+                        <div className={styles['relevant_experience_upper_left_container']}>
+                            <h1>Relevant Experience</h1>
+                        </div>
+                        <div className={styles['relevant_experience_upper_right_container']}>
+                            <button onClick={() => removeExperience(experiences, setExperiences)} className={`${styles.button} ${styles.bg_yellow}`}>-</button>
+                            <button onClick={() => addExperience(experiences, setExperiences)} className={`${styles.button} ${styles.bg_yellow}`}>+</button>
+                        </div>
                     </div>
                     <div className={styles['relevant_experience_lower_container']}>
-                            <button className={`${styles.pill} ${styles.pill_large_text}`}>Engineered a full-stack web application for a university project using React for the frontend and Node.js for the backend, implementing RESTful APIs and real-time data with WebSocket.</button>
-                            <button className={`${styles.pill} ${styles.pill_large_text}`}>Contributed to an open-source machine learning project on GitHub, enhancing algorithms and optimizing data processing pipelines in Python.</button>
-                            <button className={`${styles.pill} ${styles.pill_large_text}`}>Led a team in a national coding hackathon to develop a mobile app prototype that won second place for its innovative use of AI in educational technology.</button>
+                        {experiences.map((experience, index) => (
+                            <button key={index} className={`${styles.pill}`}>{experience}</button>
+                        ))}
                     </div>                 
-                </div> 
+                </div>  
             </div>
             <div className={styles['nested_container_right']}>
                 <div className={styles['personal_info_container']}>
                     <div className={styles['personal_info_upper_container']}>
-                        <h1>Personal Info</h1>
-                        <button className={`${styles.button} ${styles.bg_yellow}`}>+</button>
+                        <div className={styles['personal_info_upper_left_container']}>
+                            <h1>Personal Info</h1>
+                        </div>
+                        <div className={styles['personal_info_upper_right_container']}>
+                            <button onClick={() => removePersonalInfo(personal_infos, setPersonal_Infos)} className={`${styles.button} ${styles.bg_yellow}`}>-</button>
+                            <button onClick={() => addNewPersonalInfo(personal_infos, setPersonal_Infos)} className={`${styles.button} ${styles.bg_yellow}`}>+</button>
+                        </div>
                     </div>
                     <div className={styles['personal_info_lower_container']}>
-                            <button className={`${styles.pill}`}>Name: First Last</button>
-                            <button className={`${styles.pill}`}>GPA: 5.0</button>
-                            <button className={`${styles.pill}`}>Occupation: Student</button>
+                        {personal_infos.map((personal_info, index) => (
+                            <button key={index} className={`${styles.pill}`}>{personal_info}</button>
+                        ))}
                     </div>        
                 </div>
             </div>
--- app/components/ProfilePageFunctions.jsx
+++ app/components/ProfilePageFunctions.jsx
@@ -3,12 +3,74 @@
 export const addNewSkill = (skills, setSkills) => {
   const newSkill = prompt("Enter the name of the new skill:");
 
-  const newSkills = skills.concat(newSkill);
-
-  setSkills(newSkills);
+  if (newSkill) {
+    const newSkills = skills.concat(newSkill);
+    setSkills(newSkills);
+  }
 };
 
 export const removeSkill = (skills, setSkills) => {
-    setSkills(skills.slice(0, skills.length - 1));
+    if (skills.length != 0){
+      setSkills(skills.slice(0, skills.length - 1));  
+    }
+};
+
+export const addNewCourse = (courses, setCourses) => {
+  const newCourse = prompt("Enter the name of the new course:");
+
+  if (newCourse) {
+    const newCourses = courses.concat(newCourse);
+    setCourses(newCourses);
+  }
+};
+
+export const removeCourse = (courses, setCourses) => {
+    if (courses.length != 0){
+      setCourses(courses.slice(0, courses.length - 1));  
+    }
 };
   
+export const addYear = (years, setYears) => {
+  const newYear = prompt("Enter expected graduation year:");
+
+  if (newYear && years.length == 0) {
+    const newYears = years.concat(newYear);
+    setYears(newYears);
+  }
+};
+
+export const removeYear = (years, setYears) => {
+  if (years.length != 0){
+    setYears(years.slice(0, years.length - 1));  
+  }
+};
+
+export const addExperience = (experiences, setExperiences) => {
+  const newExperience = prompt("Enter relevant experience:");
+
+  if (newExperience) {
+    const newExperiences = experiences.concat(newExperience);
+    setExperiences(newExperiences);
+  }
+};
+
+export const removeExperience = (experiences, setExperiences) => {
+  if (experiences.length != 0){
+    setExperiences(experiences.slice(0, experiences.length - 1));  
+  }
+};
+
+export const addNewPersonalInfo = (personal_infos, setPersonal_Infos) => {
+  const newPersonalInfo = prompt("Enter personal info:");
+
+  if (newPersonalInfo) {
+    const newPersonal_Infos = personal_infos.concat(newPersonalInfo);
+    setPersonal_Infos(newPersonal_Infos);
+  }
+};
+
+export const removePersonalInfo = (personal_infos, setPersonal_Infos) => {
+    if (personal_infos.length != 0){
+      setPersonal_Infos(personal_infos.slice(0, personal_infos.length - 1));  
+    }
+};
