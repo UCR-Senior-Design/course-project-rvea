d3adb84667a8b766f56d5f09652849665df978ac
ethiopiae <ethanjfoxy@gmail.com>
2024-03-09 23:25:30 -0800

    saving new students. toggle is giving issues saving professors

--- app/components/RegisterForm.jsx
+++ app/components/RegisterForm.jsx
@@ -1,26 +1,32 @@
 'use client';
 
+import { saveNewUser, authenticate } from '../lib/actions';
 import { useState } from 'react';
-import { useRouter } from 'next/navigation'; // Import from next/navigation instead of next/router
-import styles from '../styles/LoginForm.module.css'; // Import styles from LoginForm.module.css
+import { useRouter } from 'next/router';
+import styles from '../styles/LoginForm.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
 
-import Link from 'next/link'; 
-
-export default function RegisterForm() {
-    const router = useRouter();
+export default function RegisterForm({ router }) {
     const [formData, setFormData] = useState({
         fullName: '',
         email: '',
         password: '',
     });
+    const [toggleState, setToggleState] = useState(false);
+    const [title, setTitle] = useState('Register - Student');
 
-    const handleRegister = async (e) => {
-        e.preventDefault(); // Prevent default form submission behavior
-        // Perform registration logic here
-        // For example, you can send the form data to your server
-        // and handle the registration process
-        // After successful registration, navigate to another page
-        router.push('/'); // Redirect to the home page
+    const handleRegister = async (event) => {
+        event.preventDefault();
+
+        try {
+            console.log('Submitting form...');
+            await saveNewUser(formData, toggleState ? 'professor' : 'student');
+            await authenticate(formData);
+            router.push('/');
+        } catch (error) {
+            console.error('Error registering user:', error);
+        }
     };
 
     const handleChange = (e) => {
@@ -31,18 +37,59 @@ export default function RegisterForm() {
         });
     };
 
+    const handleToggle = () => {
+        setToggleState(!toggleState);
+        setTitle(toggleState ? 'Register - Student' : 'Register - Professor');
+    };
+
     return (
         <>
+            <Image src="/ucr_logo.png" width="210" height="64" alt="ucr school logo" priority />
+            <div className={styles.toggleContainer}>
+            </div>
             <form onSubmit={handleRegister}>
                 <div className={styles.card}>
-                    <h2 className={styles.bold}>Register</h2>
-                    <input id="fullName" name="fullname" className={`${styles.border_rounded_8} ${styles.border_grey} ${styles.registerInput}`} placeholder="Full Name" type="text" value={formData.email} onChange={handleChange} />
-                    <input id="email" name="email" className={`${styles.border_rounded_8} ${styles.border_grey} ${styles.registerInput}`} placeholder="Email" type="text" value={formData.email} onChange={handleChange} />
-                    <input id="password" name="password" className={`${styles.border_rounded_8} ${styles.border_grey} ${styles.registerInput}`} placeholder="Password" type="password" value={formData.password} onChange={handleChange} />
-                    <button className={`${styles.button_blue} ${styles.margin_top_24}`} type="submit"> Register </button>
-                    <h5 className={styles.line_through}><span>or</span></h5>
-                    <Link href="/" className={`${styles.button_yellow} ${styles.margin_btm_24}`}>
-                    <button className={`${styles.button_yellow} ${styles.margin_btm_24}`}>Back to Login</button>
+                    <h2 className={styles.bold}>{title} <button className={styles.toggleButton} onClick={handleToggle}>Toggle</button></h2>
+                    
+                    <input
+                        id="fullName"
+                        name="fullName"
+                        className={`${styles.border_rounded_8} ${styles.border_grey} ${styles.registerInput}`}
+                        placeholder="Full Name"
+                        type="text"
+                        value={formData.fullName}
+                        onChange={handleChange}
+                    />
+                    <input
+                        id="email"
+                        name="email"
+                        className={`${styles.border_rounded_8} ${styles.border_grey} ${styles.registerInput}`}
+                        placeholder="Email"
+                        type="text"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                    <input
+                        id="password"
+                        name="password"
+                        className={`${styles.border_rounded_8} ${styles.border_grey} ${styles.registerInput}`}
+                        placeholder="Password"
+                        type="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+                    <button className={`${styles.button_blue} ${styles.margin_top_24}`} type="submit">
+                        Register Now
+                    </button>
+                    <h5 className={styles.line_through}>
+                        <span>or</span>
+                    </h5>
+                    <Link
+                        href="/"
+                        className={`${styles.button_yellow} ${styles.margin_btm_24}`}
+                        style={{ textDecoration: 'none', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                    >
+                        <p style={{ margin: '0', fontSize: '13px', color: '#FFF' }}>Back to Login</p>
                     </Link>
                 </div>
             </form>
