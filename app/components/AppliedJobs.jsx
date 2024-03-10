8dd6c61bf17f9b68aefa8d217a1cf9f78fe2a7d2
Adlai M.B <adlaimb@gmail.com>
2024-03-09 22:20:25 -0800

    get user info on applied pages

--- app/components/AppliedJobs.jsx
+++ app/components/AppliedJobs.jsx
@@ -16,12 +16,12 @@ export default function AppliedJobs(props) {
                             <div className={styles.card_top}>
                                 <img src="/cs_icon.png" alt="cs icon for cards" width="55" height="60" />
                                 <div className={styles.card_info}>
-                                    <h4>{job.positionInfo}</h4>
-                                    <h4>{job.professor}</h4>
+                                    <h4>{job.Title + "-" + job.Term + "-" + job.Position}</h4>
+                                    <h4>{job.Professor}</h4>
                                 </div>
                                 <button className={`${styles.button} ${styles.bg_yellow}`} onClick={() => { setButtonPopup(true); document.body.style.overflow = "hidden" }}>View</button>
                             </div>
-                            <p className={styles.card_description}>{job.positionDescription}</p>
+                            <p className={styles.card_description}>{job.Description}</p>
                             <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
                                 <object data="/mock_contract.pdf" type="application/pdf" width="1000" height="800"></object>
                             </Popup>
