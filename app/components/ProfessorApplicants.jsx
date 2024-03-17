3484d0cb171f7d15356f2c7036d90a12ce273dae
ethiopiae <ethanjfoxy@gmail.com>
2024-03-16 20:24:08 -0700

    fixed to accept individuals

--- app/components/ProfessorApplicants.jsx
+++ app/components/ProfessorApplicants.jsx
@@ -8,9 +8,10 @@ import { acceptStudent } from '../lib/actions';
 export default function ProfessorApplicants(props) {
     let applicants = JSON.parse(props.applicants);
     console.log(applicants);
-    const [status, setStatus] = useState();
-    const handleAccept = () => {
-        setStatus('accept')
+    const [status, setStatus] = useState({});
+    const handleAccept = (key, studentId) => {
+        setStatus({ ...status, [studentId]: 'accept' });
+        acceptStudent(key, studentId);
     }
     const handleDecline = () => {
         setStatus('decline')
@@ -39,49 +40,48 @@ export default function ProfessorApplicants(props) {
                                         </div>
 
                                         <ul key={student} className={styles.accordion}>
-                                            <li>
-                                                <input type='checkbox' name='accordion' id='first' />
-                                                <label htmlFor='first' className={styles.labels}>
-                                                    <p>{student.username}</p>
-                                                    <p>{student.degreelvl}</p>
-                                                    <p>{student.gpa}</p>
-                                                    <p>{student.applicationDate}</p>
-                                                    <p>
-                                                        <button onClick={() => setPdf(student.transcript)}>
-                                                            <Link legacyBehavior href={pdf ? `${pdf}` : ''} passHref>
-                                                                <a target="_blank">
-                                                                    <Image className={styles.image} src='/doc_icon.png' width='43' height='43'></Image>
-                                                                </a>
-                                                            </Link>
-                                                        </button>
-                                                    </p>
-                                                    {status === 'accept' || student.accepted ? <p className={styles.underline}>Accepted</p> :
-                                                        status === 'decline' ? <p className={styles.underline}>Declined</p> :
-                                                            <div className={styles.buttons}>
-                                                                <form id="accept-decline"></form>
-                                                                <button form="accept-decline" type="submit" className={`${styles.buttons} ${styles.green}`} formAction={() => {acceptStudent(key, student.id); setStatus('accept')}}>Accept</button>
-                                                                <button className={`${styles.buttons} ${styles.red}`} onClick={handleDecline}>Decline</button>
-                                                            </div>
-                                                    }
-                                                </label>
+    <li>
+        <input type='checkbox' name={`accordion-${key}-${student.username}`} id={`accordion-${key}-${student.username}`} />
+        <label htmlFor={`accordion-${key}-${student.username}`} className={styles.labels}>
+            <p>{student.username}</p>
+            <p>{student.degreelvl}</p>
+            <p>{student.gpa}</p>
+            <p>{student.applicationDate}</p>
+            <p>
+                <button onClick={() => setPdf(student.transcript)}>
+                    <Link legacyBehavior href={pdf ? `${pdf}` : ''} passHref>
+                        <a target="_blank">
+                            <Image className={styles.image} src='/doc_icon.png' width='43' height='43'></Image>
+                        </a>
+                    </Link>
+                </button>
+            </p>
+            {status[student.id] === 'accept' || student.accepted ? <p className={styles.underline}>Accepted</p> :
+                status === 'decline' ? <p className={styles.underline}>Declined</p> :
+                    <div className={styles.buttons}>
+                        <form id={`accept-decline-${key}-${student.username}`}></form>
+                        <button form={`accept-decline-${key}-${student.username}`} type="submit" className={`${styles.buttons} ${styles.green}`} onClick={() => handleAccept(key, student.id)}>Accept</button>
+                        <button className={`${styles.buttons} ${styles.red}`} onClick={handleDecline}>Decline</button>
+                    </div>
+            }
+        </label>
 
-                                                <div className={styles.content}>
-                                                    <p className={styles.bold}>Pronouns: {student.pronouns} </p>
-                                                    <p className={styles.bold}>Courses Taken: {student.courses.map((course) => course + " ")}</p>
-                                                    <p className={`${styles.bold} ${styles.skills}`}>Skills: {student.skills.map((skill) => skill + " ")}</p>
-                                                    <p className={`${styles.bold} ${styles.resume_container}`}>Resume:
-                                                        <button onClick={() => setPdf(student.resume)}>
-                                                            <Link legacyBehavior href={pdf ? `${pdf}` : ''} passHref>
-                                                                <a target="_blank">
-                                                                    <Image className={styles.image} src='/doc_icon.png' width='43' height='43'></Image>
-                                                                </a>
-                                                            </Link>
-                                                        </button>
-                                                    </p>
-
-                                                </div>
-                                            </li>
-                                        </ul>
+        <div className={styles.content}>
+            <p className={styles.bold}>Pronouns: {student.pronouns} </p>
+            <p className={styles.bold}>Courses Taken: {student.courses.map((course) => course + " ")}</p>
+            <p className={`${styles.bold} ${styles.skills}`}>Skills: {student.skills.map((skill) => skill + " ")}</p>
+            <p className={`${styles.bold} ${styles.resume_container}`}>Resume:
+                <button onClick={() => setPdf(student.resume)}>
+                    <Link legacyBehavior href={pdf ? `${pdf}` : ''} passHref>
+                        <a target="_blank">
+                            <Image className={styles.image} src='/doc_icon.png' width='43' height='43'></Image>
+                        </a>
+                    </Link>
+                </button>
+            </p>
+        </div>
+    </li>
+</ul>
                                     </>
                                 )
                             })}
